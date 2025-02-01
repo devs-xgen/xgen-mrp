e36353d8956ddd9f8bfc291475e005a64dc6d39c
Marvs2 <marvinrvillanueva@iskolarngbayan.edu.ph>
2025-02-01 12:12:08 +0800

    purchase order line - not finished

--- src/app/admin/(protected)/purchase-orderline/loading.tsx
+++
@@ -1,7 +0,0 @@
-// src/app/admin/(protected)/products/loading.tsx
-import { Card, CardContent, CardHeader } from "@/components/ui/card"
-import { PageSkeleton } from "@/components/shared/page-skeleton"
-
-export default function Loading() {
-    return <PageSkeleton />
-  }
